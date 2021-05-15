e987548c9235da30dcb4c05d99c0ed46daf279c2
hyerim0414 <hearim0414@naver.com>
2021-05-15 16:32:18 +0900

    Summer/Winter Coding(~2018)

--- Programmers_배달.py
+++ Programmers_배달.py
@@ -1,7 +1,7 @@
 """
 - shortest path search algorithm 중 Dijkstra's algorithm 이용
 - bfs 인접 노드 탐색, minheap 이용
-- 제한사항 : 두 마을을 연결하는 도로는 여러개 있을 수 있다. 따라서, 이중 딕셔너리 사용하지 않음
+- 제한사항 : 두 마을을 연결하는 도로는 여러개 있을 수 있다. => 이중 딕셔너리 사용하지 않음
 
 """
 
@@ -11,7 +11,7 @@ def GraphPath(N,road,K):
     INF=500000
     #그래프 생성
     G={}
-    minheap=[[1,0]]
+    minheap=[(0,1)] #(distance, node)
     distance=[INF]*(N+1)
     distance[1]=0
     for i in range(1,N+1):
@@ -22,12 +22,12 @@ def GraphPath(N,road,K):
         G[v2].append([v1,r[2]])
 
     while(minheap!=[]):
-        node=heapq.heappop(minheap)
-        for adjacent, cost in G[node[0]]:
+        cur_cost, cur_node=heapq.heappop(minheap)
+        for next_node,next_cost in G[cur_node]:
             #인접 노드의 cost가 update되면 push
-            if((node[1]+cost)<distance[adjacent]):
-                distance[adjacent]=node[1]+cost
-                heapq.heappush(minheap,[adjacent,distance[adjacent]])
+            if((cur_cost+next_cost)<distance[next_node]):
+                distance[next_node]=cur_cost+next_cost
+                heapq.heappush(minheap,(distance[next_node],next_node))
 
     for i in range(len(distance)):
         if(distance[i]<=K): count+=1
