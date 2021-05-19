c9b7a35c52195ec222231420cd1d4ea63062876a
hyerim0414 <hearim0414@naver.com>
2021-05-19 13:19:42 +0900

    월간 코드 챌린지 시즌1

--- Programmers_삼각달팽이.py
+++ Programmers_삼각달팽이.py
@@ -1,4 +1,5 @@
 '''
+- 아래->오른쪽->대각선 순으로 방향이 변경됨
 - 각 방향마다 n번 -> n-1번 -> ... -> 1번 까지 움직임
 - 3가지 방향이 rotation 
 '''
@@ -8,7 +9,7 @@ def solution(n):
     table=[[0]*n for i in range(n)] #0으로 채워진 nxn 2차원 배열
     N=1
     p_x,p_y=-1,0 #이전 좌표
-    for direction in range(n): #아래->오른쪽->대각선 순으로 방향 변경
+    for direction in range(n): 
         if(direction%3==0):
             p,q=1,0
         elif(direction%3==1):
