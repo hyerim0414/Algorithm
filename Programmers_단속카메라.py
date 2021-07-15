98bc30890c890637bad56484bf00e6f49e5bfe28
hyerim0414 <hearim0414@naver.com>
2021-07-15 23:54:38 +0900

    탐욕법(Greedy)

--- Programmers_단속카메라.py
+++ Programmers_단속카메라.py
@@ -2,8 +2,10 @@ def solution(routes):
     answer = 0
     routes.sort(key=lambda x:x[1])
     cam=routes[0][0]-1
+
     for route in routes:
         if(cam<route[0]):
             answer+=1
             cam=route[1]
+            
     return answer
