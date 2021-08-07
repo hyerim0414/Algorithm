08cfddccc6930bcc15d417ac926a7474213fb096
hyerim0414 <hearim0414@naver.com>
2021-08-07 23:50:05 +0900

    2805번

---
+++ Baekjoon_나무자르기.py
@@ -0,0 +1,26 @@
+#2805번
+
+import sys
+
+def bi_search(start,end,tree_length,M):
+    mid=(start+end)//2
+    if(start>end):
+        return end
+    
+    count=0
+    for tree in tree_length:
+        if(tree>mid): #잘린 나무 길이
+            count+=(tree-mid)  
+    
+    if(count<M):
+        return bi_search(start,mid-1,tree_length,M)
+
+    elif(count>=M):
+        return bi_search(mid+1,end,tree_length,M)
+    
+
+
+N,M= map(int,sys.stdin.readline().split())
+tree_length=list(map(int,sys.stdin.readline().split()))
+
+print(bi_search(0,max(tree_length),tree_length,M))
