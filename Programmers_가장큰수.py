251c39eaed3668fc88705a0f686e8652b9cb551d
hyerim0414 <hearim0414@naver.com>
2021-12-06 00:06:04 +0900

    정렬

--- Programmers_가장큰수.py
+++ Programmers_가장큰수.py
@@ -1,31 +1,18 @@
 #정렬
 
+'''
+- 1,000 이하의 숫자들 비교를 위해 *3을 해줌
+- 0만 들어왔을 때, 0을 출력하기 위해 예외사항 추가
+'''
+
 def solution(numbers):
-    new_arr=[]
-    result=""
-    for num in numbers:
-        new_arr.append(str(num))
-    new_arr=sorted(new_arr)
-    
-    
-    while(new_arr!=[]):
-        split_same=[]
-        N=new_arr[-1][0] #맨 앞 숫자
+    result=''
+    if(sorted(numbers)[-1]==0):
+        return '0'
+    else:
         
-        split_same.append(new_arr.pop())
-        while(new_arr!=[] and N==new_arr[-1][0]):
-            split_same.append(new_arr.pop())
-            continue
-        
-        i=0
-        while(i<(len(split_same)-1)):
-            j=i+1
-            N=len(split_same[i])
-            if(N==split_same[j+1]):
-                while(j<len(split_same)-1) and N==len(split_same[j+1]):
-                      j+=1
-                split_same[i:j+1]=sorted(split_same,reverse=True)
-            i=j+1
-        for k in split_same:
-            result+=k
-    return result
+        new_arr=[[str(x),str(x)*3] for x in numbers]
+        new_arr=sorted(new_arr,key=lambda x: x[1], reverse=True)
+        for i in new_arr:
+            result+=i[0]
+        return result
