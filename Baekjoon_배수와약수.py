c0cf44df458f734d9d954ec5d65ea4dc927302ca
hyerim0414 <hearim0414@naver.com>
2021-12-10 18:42:24 +0900

    5086

---
+++ Baekjoon_배수와약수.py
@@ -0,0 +1,15 @@
+#5086번
+
+import sys
+
+while(True):
+    n,m=map(int,sys.stdin.readline().split())
+    if(n==0 and m==0):
+        break
+    else:
+        if(m%n==0):
+            print("factor")
+        elif(n%m==0):
+            print("multiple")
+        else:
+            print("neither")
