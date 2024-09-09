dab22405254d025aa0d822c750567c0180cf9e4a
HyerimPark <hearim0414@naver.com>
2024-09-09 21:54:42 +0900

    240909

---
+++ Baekjoon_퇴사.py
@@ -0,0 +1,21 @@
+# 240909
+
+import sys
+
+N = int(sys.stdin.readline())
+T = []
+P = []
+cur =0
+price = 0 
+
+table = [list(map(int,sys.stdin.readline().split(" "))) for _ in range(N)]
+res = [0 for i in range(N+1)] # 맨 마지막 1일 걸리는 스케줄에 대해 가능하기 떄문
+
+for i in range(N-1,-1,-1):
+    Ti, Pi = table[i]
+    if(i+Ti > N):
+        continue
+    else:
+        res[i] = max(res[i],Pi+res[i+Ti])
+    
+print(max(res))
